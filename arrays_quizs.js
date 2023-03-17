d7bd96ac33e10811281507731ee4473a2f6c8fe0
student <joycemumbiiwanjiku@gmail.com>
2023-03-17 19:27:55 +0300

    assign

---
+++ arrays_quizs.js
@@ -0,0 +1,52 @@
+
+// Arrays
+// Find the last element of the following arrays.
+// arr1 = [3,7,34,90,12]
+let arrayOne=[3,7,34,90,12];
+let array= arrayOne.slice(-1);
+console.log(array);
+
+// arr2 = [true, "green", "where",12,56]/
+let arrayTwo=[true,"green","where",12,56,];
+let arry= arrayTwo.slice(-1);
+console.log(arry)
+// Write a JS program that will join the following array elements into a string
+// myPets = ["Cow", "Python", "Snake", "Dog"];
+let myPets = ["Cow", "Python", "Snake", "Dog"];
+console.log(myPets.join());
+
+
+// Write a JS script to sort the following array items
+// var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];//
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+arr3.sort();
+console.log(arr3);
+
+
+// Write a program to remove duplicates from the following array.
+// Console the array without duplicates, and console another array that only contains the duplicates
+// var arr = ["apple", "mango", "apple",
+            // "orange", "mango", "mango"];
+            var arr = ["apple", "mango", "apple","orange", "mango", "mango"]
+            const fruits=[]
+            arr.forEach(element => {
+                if(!fruits.includes(element)){
+                    fruits.push(element)
+                    console.log()
+                }
+                
+            });
+            console.log(fruits)
+            
+            const four=arry.sort()
+            const x=y. reverse
+            let[a, ...b]=y;
+            console.log(b)
+            
+            
+// Write a JS script to search for the following word in the array.///
+// 4
+// If the word is present, console it else console "the search word was not found"/
+//let arr5 = ["the", "way", "x", 4, 23];
+// Write a JS script to sort the following string
+// /'let word = "lufituaeb"
